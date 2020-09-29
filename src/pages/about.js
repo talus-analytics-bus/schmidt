6c1f980b412ab4c9064c54c0705861dd49e3986d
mevanmaele <mvanmaele@talusanalytics.com>
2020-09-29 10:22:29 -0400

    Add skeleton contact and about pages

---
+++ src/pages/about.js
@@ -0,0 +1,16 @@
+// 3rd party components
+import React, { useState, useEffect } from 'react'
+
+// local components
+import Layout from '../components/Layout/Layout'
+import SEO from '../components/seo'
+import { PrimaryButton } from '../components/common'
+
+// styles and assets
+import styles from '../components/About/about.module.scss'
+
+const About = ({}) => {
+  return <h2>About</h2>
+}
+
+export default About
---
+++ src/pages/contact.js
@@ -0,0 +1,16 @@
+// 3rd party components
+import React, { useState, useEffect } from 'react'
+
+// local components
+import Layout from '../components/Layout/Layout'
+import SEO from '../components/seo'
+import { PrimaryButton } from '../components/common'
+
+// styles and assets
+import styles from '../components/Contact/contact.module.scss'
+
+const Contact = ({}) => {
+  return <h2>Contact</h2>
+}
+
+export default Contact
