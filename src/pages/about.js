c2c1f3ea70e0994e7c857a3460af1acebfbad0ed
mevanmaele <mvanmaele@talusanalytics.com>
2020-09-29 10:41:16 -0400

    Finish adding skeletons for contact and about pages

--- src/pages/about.js
+++ src/pages/about.js
@@ -9,8 +9,38 @@ import { PrimaryButton } from '../components/common'
 // styles and assets
 import styles from '../components/About/about.module.scss'
 
+// local utility functions
+import { withBookmarkedIds } from '../components/misc/Util'
+
 const About = ({}) => {
-  return <h2>About</h2>
+  // STATE  // --------------------------------------------------------------//
+  // is page loaded yet? show nothing until it is
+  const [loading, setLoading] = useState(true)
+
+  // ids of bookmarked items to count for nav
+  const [bookmarkedIds, setBookmarkedIds] = useState(null)
+
+  // EFFECT HOOKS // -------—-------—-------—-------—-------—-------—-------—//
+  // get bookmarked ids initially
+  useEffect(() => {
+    if (bookmarkedIds === null)
+      withBookmarkedIds({ callback: setBookmarkedIds })
+    setLoading(false)
+  }, [bookmarkedIds])
+
+  // JSX
+  return (
+    <Layout
+      page={'about'}
+      loading={loading}
+      bookmarkCount={loading ? null : bookmarkedIds.length}
+    >
+      <SEO title="About" />
+      <div className={styles.about}>
+        <h2>About</h2>
+      </div>
+    </Layout>
+  )
 }
 
 export default About
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -6,11 +6,41 @@ import Layout from '../components/Layout/Layout'
 import SEO from '../components/seo'
 import { PrimaryButton } from '../components/common'
 
+// local utility functions
+import { withBookmarkedIds } from '../components/misc/Util'
+
 // styles and assets
 import styles from '../components/Contact/contact.module.scss'
 
 const Contact = ({}) => {
-  return <h2>Contact</h2>
+  // STATE  // --------------------------------------------------------------//
+  // is page loaded yet? show nothing until it is
+  const [loading, setLoading] = useState(true)
+
+  // ids of bookmarked items to count for nav
+  const [bookmarkedIds, setBookmarkedIds] = useState(null)
+
+  // EFFECT HOOKS // -------—-------—-------—-------—-------—-------—-------—//
+  // get bookmarked ids initially
+  useEffect(() => {
+    if (bookmarkedIds === null)
+      withBookmarkedIds({ callback: setBookmarkedIds })
+    setLoading(false)
+  }, [bookmarkedIds])
+
+  // JSX
+  return (
+    <Layout
+      page={'contact'}
+      loading={loading}
+      bookmarkCount={loading ? null : bookmarkedIds.length}
+    >
+      <SEO title="About" />
+      <div className={styles.contact}>
+        <h2>Contact</h2>
+      </div>
+    </Layout>
+  )
 }
 
 export default Contact
