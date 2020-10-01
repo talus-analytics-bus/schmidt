52a1ede0fb46562035e457a46b79671406254e57
mevanmaele <mvanmaele@talusanalytics.com>
2020-10-01 13:02:32 -0400

    Add wrapper component to browser file

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,5 +4,21 @@
  * See: https://www.gatsbyjs.org/docs/browser-apis/
  */
 
+// 3rd party packages
+import React from 'react'
+
 // You can delete this file if you're not using it
 import './src/assets/styles/fonts.css'
+
+// https://www.freecodecamp.org/news/keeping-state-between-pages-with-local-state-in-gatsby-js/
+// gatsby-browser.js
+// Import the component at the top of the file
+const Wrapper = ({ props, children }) => (
+  <div {...props} data-type={'wrapper'}>
+    {children}
+  </div>
+)
+
+export const wrapPageElement = ({ element, props }) => (
+  <Wrapper {...props}>{element}</Wrapper>
+)
