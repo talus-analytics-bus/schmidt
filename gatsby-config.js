266e0996136ca40b2b20f3cebadeb902e072f734
Nate Bennett <nbennett@talusanalytics.com>
2020-11-11 11:52:57 -0700

    added google analytics (upgraded) and cookie consent

--- gatsby-config.js
+++ gatsby-config.js
@@ -41,6 +41,17 @@ module.exports = {
         generateRoutingRules: false,
       },
     },
+    {
+      resolve: `gatsby-plugin-gtag`,
+      options: {
+        // your google analytics tracking id
+        trackingId: `G-KHMJG9CZYH`,
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // enable ip anonymization
+        anonymize: true,
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
