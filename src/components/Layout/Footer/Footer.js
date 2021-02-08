4f8139f2f250515966327680f953f0072af475a1
Nate Bennett <nbennett@talusanalytics.com>
2021-02-08 09:44:37 -0700

    Attempt at removing ghost footer border

--- src/components/Layout/Footer/Footer.js
+++ src/components/Layout/Footer/Footer.js
@@ -18,33 +18,29 @@ import idea from '../../../assets/images/logo-title.png'
 
 const Footer = () => {
   return (
-    <div className={styles.footerContainer}>
-      <div className={styles.footer}>
-        <div className={styles.content}>
-          <a href={'https://ghssidea.org'} target="_blank">
-            <img
-              src={idea}
-              alt={
-                'International Disease and Events Analysis'
-              }
-            ></img>
-          </a>
-          <a href={'https://ghss.georgetown.edu/'} target="_blank">
-            <img
-              src={georgetown}
-              alt={
-                'Georgetown University Center for Global Health Science and Security'
-              }
-            ></img>
-          </a>
-          <a href={'https://talusanalytics.com/'} target="_blank">
-            <img
-              className={styles.talus}
-              src={talus}
-              alt={'Talus Analytics'}
-            ></img>
-          </a>
-        </div>
+    <div className={styles.footer}>
+      <div className={styles.content}>
+        <a href={'https://ghssidea.org'} target="_blank">
+          <img
+            src={idea}
+            alt={'International Disease and Events Analysis'}
+          ></img>
+        </a>
+        <a href={'https://ghss.georgetown.edu/'} target="_blank">
+          <img
+            src={georgetown}
+            alt={
+              'Georgetown University Center for Global Health Science and Security'
+            }
+          ></img>
+        </a>
+        <a href={'https://talusanalytics.com/'} target="_blank">
+          <img
+            className={styles.talus}
+            src={talus}
+            alt={'Talus Analytics'}
+          ></img>
+        </a>
       </div>
     </div>
   )
