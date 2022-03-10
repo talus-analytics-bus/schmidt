5ad78c23989883ce113eaa76562da627be96f8e4
mvanmaele <mvanmaele@talusanalytics.com>
2022-03-10 16:53:13 -0500

    Add test

--- src/components/Search/Options/content/YearsCheckboxSet/YearsCheckboxSet.spec.jsx
+++ src/components/Search/Options/content/YearsCheckboxSet/YearsCheckboxSet.spec.jsx
@@ -1,34 +1,45 @@
-import React from 'react'
-import { fireEvent, render, screen } from '@testing-library/react'
+import React, { useState } from 'react'
+import { fireEvent, render } from '@testing-library/react'
 import YearsCheckboxSet from './YearsCheckboxSet'
 import styles from './options.module.scss'
 
-it('YearsCheckboxSet changes the text after click', () => {
-  const { container, queryByText, queryAllByText } = render(
-    <YearsCheckboxSet
-      filters={{ "years": ["2019"] }}
-      field={'years'}
-      setFromYear={() => ''}
-      setToYear={() => ''}
-      setFilters={() => ''}
-      curFilterSectionData={{
-        label: 'years',
-        field: 'years',
-        choices: ['2020', '2019', '2018'].map(value => { return { value, label: value } }),
-        iconName: null,
-      }}
-      {...{ styles }}
-    />
+const YearsCheckboxSetWrapper = () => {
+  const [filters, setFilters] = useState({})
+  return <YearsCheckboxSet
+    filters={filters}
+    field={'years'}
+    setFromYear={() => ''}
+    setToYear={() => ''}
+    curFilterSectionData={{
+      label: 'years',
+      field: 'years',
+      choices: ['2020', '2019', '2018'].map(value => {
+        return { value, label: value }
+      }),
+      iconName: null,
+    }}
+    {...{ setFilters, styles }}
+  />
+};
+
+it('YearsCheckboxSet checkbox checks after click', () => {
+  const { container, queryAllByText } = render(
+    <YearsCheckboxSetWrapper />
   )
 
   expect(queryAllByText(/2019/)).toBeTruthy()
   expect(queryAllByText(/1900/i)).toHaveLength(0)
-  const checkbox = container.querySelector('input[value="2019"]')
-  expect(checkbox).toBeTruthy()
-  // expect(queryByText(/1900/i)).toBeFalsy()
-  // const checkbox = getByText(/2019/)
-  // fireEvent.click(checkbox)
-  // screen.debug()
-
-  // expect(queryByLabelText(/on/i)).toBeTruthy()
+  const cbValues = ['2019', 'custom']
+  cbValues.forEach(cbValue => {
+    const cbLabel = container.querySelector(`label[for="${cbValue}"]`)
+    const cb = cbLabel.querySelector(`input[value="${cbValue}"]`)
+    const cbIcon = cbLabel.querySelector('span:first-child')
+    expect(cbLabel).toBeTruthy()
+    expect(cbIcon).toBeTruthy()
+    expect(cb).toBeTruthy()
+    expect(cbIcon.className).toContain('check')
+    expect(cbIcon.className).not.toContain('visible')
+    fireEvent.click(cb)
+    expect(cbIcon.className).toContain('visible')
+  })
 })
