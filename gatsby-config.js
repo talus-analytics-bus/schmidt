176ad757c11d80ba582d8eb9805180a33c08e6cf
Ryan Zimmerman <rzimmerman@talusanalytics.com>
2022-10-20 12:21:22 -0300

    Adding plausible analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -50,6 +50,12 @@ module.exports = {
         anonymize: true,
       },
     },
+    {
+      resolve: `gatsby-plugin-plausible`,
+      options: {
+        domain: `healthsecuritynet.org`,
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
